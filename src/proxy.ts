8b2919def1127175a6c0b005fd12c8b7470b9edd
jahidhasan909 <jjjjahidhasan999@gmail.com>
2026-07-16 01:24:02 +0600

    proxy.ts added

---
+++ src/proxy.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { auth } from './lib/auth'
+import { headers } from 'next/headers'
+
+export async function proxy(request: NextRequest) {
+
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
+
+
+    if (!session) {
+
+        return NextResponse.redirect(new URL('/login', request.url))
+    }
+
+
+
+}
+
+export const config = {
+    matcher: ['/reports/:id','/dashboard','/dashboard/admin','/dashboard/citizen','/dashboard/officer','/funding'],
+}
